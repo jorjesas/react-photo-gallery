53b71d9b694252af48f47a41470572c3fe9b7e78
George Sas <george.sas@pitechnologies.ro>
2018-01-05 18:27:18 +0200

    Upload image stuff

--- app/containers/UploadImagePage/index.js
+++ app/containers/UploadImagePage/index.js
@@ -8,6 +8,8 @@ import RaisedButton from 'material-ui/RaisedButton';
 import Select from 'components/Select';
 import Input from 'components/Input';
 
+import { styles } from './styles.css';
+
 const style = {
     margin: 15,
   };
@@ -17,15 +19,47 @@ export class UploadImagePage extends React.PureComponent {
     constructor(props){
         super(props);
         this.state={
-          filesPreview:[],
-          filesToBeSent:[],
-          printcount:10,
+          albums: [],
+          filesPreview: [],
+          filesToBeSent: [],
+          printcount: 1,
+          imageAlbumId: '',
+          imageTitle: '',
+          imageDescription: ''
         }
       }
     
+    componentDidMount() {
+        let getAlbumsAPI = {
+          link: 'https://api.imgur.com/3/account/jorje12/albums/',
+          method: 'GET',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': 'Client-ID 9a8bbc05084cd11'
+          }
+        }
+    
+        fetch(getAlbumsAPI.link, {
+          method: getAlbumsAPI.method,
+          headers: getAlbumsAPI.headers
+        })
+        .then(results => {
+          return results.json();
+        })
+        .then(data => {
+          console.log(data);
+          let albumsContainer = data.data.map((album, index) => {
+            return (
+              {id:album.id, title:album.title}
+            );
+          })
+          this.setState({albums: albumsContainer});
+          console.log("albums", this.state.albums);
+        })
+    }
 
     onDrop(acceptedFiles, rejectedFiles) {
-        // console.log('Accepted files: ', acceptedFiles[0].name);
         var filesToBeSent=this.state.filesToBeSent;
         if(filesToBeSent.length < this.state.printcount){
           filesToBeSent.push(acceptedFiles);
@@ -50,27 +84,41 @@ export class UploadImagePage extends React.PureComponent {
         }
      }
 
+     onChange = (e) => {
+      const state = this.state
+      state[e.target.name] = e.target.value;
+      this.setState(state);
+    }
+
      handleClick(event){
-        // console.log("handleClick",event);
-        // var self = this;
-        // if(this.state.filesToBeSent.length>0){
-        //   var filesArray = this.state.filesToBeSent;
-        //   var req = request.post(apiBaseUrl+'fileupload');
-        //   for(var i in filesArray){
-        //       // console.log("files",filesArray[i][0]);
-        //       req.attach(filesArray[i][0].name,filesArray[i][0])
-        //   }
-        //   req.end(function(err,res){
-        //     if(err){
-        //       console.log("error ocurred");
-        //     }
-        //     console.log("res",res);
-        //     alert("File printing completed")
-        //   });
-        // }
-        // else{
-        //   alert("Please upload some files first");
-        // }
+        console.log("handleClick",event);  
+        let formData = {};
+
+        var self = this;
+        if(this.state.filesToBeSent.length > 0){
+          var filesArray = this.state.filesToBeSent;
+          // var req = request.post(apiBaseUrl+'fileupload');
+          formData = {image:filesArray[0][0], album:this.state.imageAlbumId, title:this.state.imageTitle, description:this.state.imageDescription};
+          console.log(formData);
+
+          // for(var i in filesArray){
+          //     // console.log("files",filesArray[i][0]);
+          //     req.attach(filesArray[i][0].name,filesArray[i][0])
+          // }
+        }
+        else{
+          alert("Please upload some files first");
+        }
+
+        // let postUploadAPI = {
+        //   link: 'https://api.imgur.com/3/image',
+        //   method: 'POST',
+        //   headers: {
+        //     'Content-Type': 'multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW',
+        //     'Authorization': 'Client-ID 9a8bbc05084cd11'
+        //   },
+        //   body: formData
+        // }   
       }
 
 render() {
@@ -80,13 +128,11 @@ render() {
 
           <center>
             <form>  
-            <div>
-                You can upload up to {this.state.printcount} files at a time.
-            </div>
-
-            <Dropzone onDrop={(files) => this.onDrop(files)}>
-                    <div>Try dropping some files here, or click to select files to upload.</div>
-            </Dropzone>
+              <div style={style}>
+                <Dropzone onDrop={(files) => this.onDrop(files)}>
+                  <div>Try dropping some files here, or click to select files to upload.</div>
+                </Dropzone>
+              </div>
             <div>
             Files to be printed are:
             {this.state.filesPreview}
@@ -95,9 +141,9 @@ render() {
             {this.state.printingmessage}
             </div>
 
-            <Select albums={[{id:"1", title:"Album 1"}, {id:"2", title:"Album 2"}]}></Select>
-            <Input title = 'Add a title' style={{width:"40%"}}></Input>
-            <Input title = 'Add a description'></Input>
+            <Select albums={this.state.albums} name="album" value={this.state.imageAlbumId} onChange={this.onChange}></Select>
+            <Input title = 'Add a title' style={{width:"40%"}} name="title" value={this.state.imageTitle} onChange={this.onChange}></Input>
+            <Input title = 'Add a description' name="description" value={this.state.imageDescription} onChange={this.onChange}></Input>
 
             <MuiThemeProvider>
                 <RaisedButton label="Upload Files" primary={true} style={style} onClick={(event) => this.handleClick(event)}/>
